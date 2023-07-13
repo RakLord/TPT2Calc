11240062fcf185b7cb0b693d96d28795f917f003
cratorrex <61958923+cratorrex@users.noreply.github.com>
2023-07-13 18:27:12 +0800

    Update data.js

--- data.js
+++ data.js
@@ -9,5 +9,18 @@ const difficulties = {
 
 const regions = {
     FOREST: 0,
-
+    DESERT: 1,
+    WINTER: 2,
+    UNDERGROUND: 3,
+    VOLCANO: 4,
+    HIGH_MOUNTAIN: 5,
+    JUNGLE: 6,
+    METALLIC_RUINS: 7,
+    BEACH: 8,
+    OCEAN: 9,
+    NEUTRAL: 10,
+    DARK_REALM: 11,
+    HEAVEN: 12,
+    UNIVERSE: 13,
+    CHAOS: 14
 }
